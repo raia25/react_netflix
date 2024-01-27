bef13dcb75d5813bc2def616a93a0d12d76c4f0e
Antonio <antoninoraia.online@gmail.com>
2024-01-27 22:45:55 +0100

    Prima versione setup tailwind e fonts

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='uppercase'>
+      <h1 className='font-nsans-light'>netflix</h1>
+      <h1 className='font-nsans-medium'>netflix</h1>
+      <h1 className='font-nsans-regular'>netflix</h1>
+      <h1 className='font-nsans-bold'>netflix</h1>
+    </div>
+  )
+}
+
+
+export default App;
